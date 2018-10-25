adfa4a862de69700b06893cf91080d45a4010989
yousef19-meet <yousef19@meet.mit.edu>
2018-10-25 08:49:44 -0700

    hi

--- app.py
+++ app.py
@@ -3,7 +3,12 @@ app = Flask(__name__)
 
 @app.route('/')
 def home_page():
-    return None
+    return render_template("home.html")
+
+@app.route('/shop')
+def shop_page():
+    return render_template("shop.html")
+
 
 if __name__ == '__main__':
    app.run(debug = True)
